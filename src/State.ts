f1589d959c249f14202362b50a00c0a40af5c629
Anwesh43 <anweshthecool0@gmail.com>
2020-01-12 20:48:38 +0530

    adding Animator class

---
+++ src/State.ts
@@ -0,0 +1,24 @@
+const scGap : number = 0.02
+class State {
+
+    dir : number = 0
+    prevScale : number = 0
+    scale : number = 0
+
+    update(cb : Function) {
+        this.scale += this.dir * scGap
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
